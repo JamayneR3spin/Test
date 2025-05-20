f72931c71a5e816928e9732dcf018a4630bd61bd
JamayneR3spin <Jam@r3spin.co.uk>
2025-05-20 12:04:03 +0100

    Calculator app functions
    First stage of the calculator app, with the basic functions and defined class

---
+++ Calculator app.py
@@ -0,0 +1,17 @@
+class Calculator:
+    #add function
+    def add (self,a,b):
+        sum_result = a+b
+        return sum_result
+
+    def subtract (self,a,b):
+        subtract_result = a-b
+        return subtract_result
+
+    def multiply (self,a,b):
+        multiply_result = a*b
+        return multiply_result
+
+    def divide (self,a,b):
+        divide_result = a/b
+        return divide_result
