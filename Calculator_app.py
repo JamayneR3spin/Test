e12f1fc328156fdda0531b1bafaee6deac71fdb3
JamayneR3spin <Jam@r3spin.co.uk>
2025-06-06 11:10:12 +0100

    Design Changes + Main
    Corrected some typos and restructured the code for a cleaner look. Also included Main to run the calculator app.

--- Calculator_app.py
+++ Calculator_app.py
@@ -15,6 +15,7 @@ class Calculator:
     def calculate(self, a, b, oper: Operation):
         if oper == Operation.DIVISION and b == 0:
             raise ZeroDivisionError("Cannot divide by zero.")
+        print("-" * 30)
         return oper.value(a, b)
 
 #Main part of code, this will be where the operations are handled
@@ -22,11 +23,11 @@ class Calculator:
 def run_calculator_app():
     calc_app = Calculator()
     print("Welcome to the calculator app!")
-    print("Type exit at anytime to quit")
+    print("Type 'exit' at anytime to quit")
 
     while True:
         try:
-            num_1_str = input("Enter the first number(or 'exit')")
+            num_1_str = input("Enter the first number(or 'exit') " )
             if num_1_str.lower() == "exit":
                 break
             num1 = float(num_1_str)  # converts the string into a float... it should.
@@ -47,6 +48,7 @@ def run_calculator_app():
                 continue  # returns to the start
             # So the operation and the first number have been handled.
 
+            #Handling of the second number (need to add exit here too)
             num2_str = input("Enter second number: ")
             num2 = float(num2_str)  # Convert input to a float
 
@@ -55,17 +57,20 @@ def run_calculator_app():
 
             # 4. Display the result
             print(f"Result: {num1} {oper_string} {num2} = {result}")
+            print("-" * 30)
 
         except ValueError:
             print("Invalid number input. Please enter valid numbers.")
         except ZeroDivisionError as e:
             print(f"Error: {e}")
-        except Exception as e:  # Catch any other unexpected errors
+        except Exception as e:  # Catch any other unexpected errors Need to research Exception
             print(f"An unexpected error occurred: {e}")
 
 
-
-
+if __name__ == "__main__":
+    run_calculator_app()
+#Need to dive into this more to fully grasp how this enabled my code to work,
+# I know it's something to do with my unittest ap
 
 
 
